dcaf55c8d4828bed4e80a447459c5e5039b17fb6
Nursakinah <nursakinah.arz@gmail.com>
2022-07-18 13:53:11 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/day11wkshp/Day11wkshpApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day11wkshp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day11wkshpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day11wkshpApplication.class, args);
+	}
+
+}
